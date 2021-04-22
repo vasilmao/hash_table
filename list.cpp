cbd589bbee7aaecf7e5af34bdebd0b84d612e77e
vasilmao <vasezs@inbox.ru>
2021-04-22 23:56:12 +0300

    redacted

--- list.cpp
+++ list.cpp
@@ -29,56 +29,56 @@ void LST_Destroy(List* list) {
 
 //rdi - List*, rsi - word
 //to return: rdx:rax
-asm(".intel_syntax noprefix\n"
-".globl LST_search\n"
-    ".type LST_search, @function\n"
-    "LST_search:\n"
-    "mov r10, [rdi]\n" // bucket size
-    "shl r10, 3\n" // bucket size * word size / 2 (8)
-    "mov r8, 0\n" // bucket iterator * word size / 2 (8)
-    "mov r9, rdi\n"
-    "LstSearchLoop:\n"
-        "cmp r8, r10\n"
-        "je LstSearchLoopExit\n"
-        "mov rdi, [r9 + 16]\n" // array of value_type*
-        "mov rdi, [rdi + r8 * 2]\n" // word, not translation
-        "mov rdx, rsi\n"            // word from argument
-        "push r10\n"
-        "call [r9 + 24]\n"         // looks like pointer on eq function
-        // rax - result - equal or not
-        "pop r10\n"
-        "cmp rax, 0\n"              // 0 - false, another - true
-        "je LstSearchWrongWord\n"
-        "mov rdi, [r9 + 16]\n"     // array
-        "mov rax, [rdi + r8 * 2]\n" // word
-        "mov r9, 8\n"
-        "shl r8, 1\n" // r8 * 2
-        "add r8, r9\n" // r8 * 3
-        "mov rdx, [rdi + r8]\n"   // translation
-        "ret\n"
-        "LstSearchWrongWord:\n"
-        "add r8, 8\n"
-        "jmp LstSearchLoop\n"
-    "LstSearchLoopExit:\n"
-    "xor rax, rax\n"
-    "xor rdx, rdx\n"
-    "ret\n"
-    ".att_syntax noprefix\n"
-);
+// asm(".intel_syntax noprefix\n"
+// ".globl LST_search\n"
+//     ".type LST_search, @function\n"
+//     "LST_search:\n"
+//     "mov r10, [rdi]\n" // bucket size
+//     "shl r10, 3\n" // bucket size * word size / 2 (8)
+//     "mov r8, 0\n" // bucket iterator * word size / 2 (8)
+//     "mov r9, rdi\n"
+//     "LstSearchLoop:\n"
+//         "cmp r8, r10\n"
+//         "je LstSearchLoopExit\n"
+//         "mov rdi, [r9 + 16]\n" // array of value_type*
+//         "mov rdi, [rdi + r8 * 2]\n" // word, not translation
+//         "mov rdx, rsi\n"            // word from argument
+//         "push r10\n"
+//         "call [r9 + 24]\n"         // looks like pointer on eq function
+//         // rax - result - equal or not
+//         "pop r10\n"
+//         "cmp rax, 0\n"              // 0 - false, another - true
+//         "je LstSearchWrongWord\n"
+//         "mov rdi, [r9 + 16]\n"     // array
+//         "mov rax, [rdi + r8 * 2]\n" // word
+//         "mov r9, 8\n"
+//         "shl r8, 1\n" // r8 * 2
+//         "add r8, r9\n" // r8 * 3
+//         "mov rdx, [rdi + r8]\n"   // translation
+//         "ret\n"
+//         "LstSearchWrongWord:\n"
+//         "add r8, 8\n"
+//         "jmp LstSearchLoop\n"
+//     "LstSearchLoopExit:\n"
+//     "xor rax, rax\n"
+//     "xor rdx, rdx\n"
+//     "ret\n"
+//     ".att_syntax noprefix\n"
+// );
 
-// value_type LST_search(List* list, value_type value) {
-//     //printf("searching for %s kek %zu lol\n", value.word, list->length);
-//     for (size_t i = 0; i < list->length; ++i) {
-//         //printf("%d\n",(int)list->equality_function({"luck", NULL}, value));
-//         //printf("%d\n",(int)list->equality_function({"luck", NULL}, list->array[0]));
-//         //printf("%d\n",(int)list->equality_function(list->array[i], value));
-//         //printf("bruh %zu %s\n", i,  list->array[i].word);
-//         if (list->equality_function(list->array[i], value)) {
-//             return list->array[i];
-//         }
-//     }
-//     return NULL_WORD;
-// }
+value_type LST_search(List* list, value_type value) {
+    //printf("searching for %s kek %zu lol\n", value.word, list->length);
+    for (size_t i = 0; i < list->length; ++i) {
+        //printf("%d\n",(int)list->equality_function({"luck", NULL}, value));
+        //printf("%d\n",(int)list->equality_function({"luck", NULL}, list->array[0]));
+        //printf("%d\n",(int)list->equality_function(list->array[i], value));
+        //printf("bruh %zu %s\n", i,  list->array[i].word);
+        if (list->equality_function(list->array[i], value)) {
+            return list->array[i];
+        }
+    }
+    return NULL_WORD;
+}
 
 void LST_add(List* list, value_type value) {
     assert(list);
--- translation.cpp
+++ translation.cpp
@@ -1,35 +1,35 @@
 #include "translation.h"
 #include "stdio.h"
 
-// bool words_equal(Word word1, Word word2) {
-//     return strcmp(word1.word, word2.word) == 0;
-// }
+bool words_equal(Word word1, Word word2) {
+    return strcmp(word1.word, word2.word) == 0;
+}
 
 
 // rdi, rdx - arguments
 // damage rcx, rax, rdi, rdx
-asm(".intel_syntax noprefix\n"
-".text\n"
-".globl words_equal\n"
-    ".type words_equal, @function\n"
-    "words_equal:\n"
-    "xor rcx, rcx\n"
-    "StrcmpLoop:\n"
-        "xor rcx, rcx\n"
-        "mov cl, byte ptr [rdi]\n"
-        "cmp cl, byte ptr [rdx]\n"
-        "jne LoopExit\n"
-        "inc rdi\n"
-        "inc rdx\n"
-        "cmp cl, 0\n"
-        "jne StrcmpLoop\n"
-        "mov rax, 0xffff\n"
-        "ret\n"
-    "LoopExit:\n"
-    "xor rax, rax\n"
-    "ret\n"
-    ".att_syntax noprefix\n"
-);
+// asm(".intel_syntax noprefix\n"
+// ".text\n"
+// ".globl words_equal\n"
+//     ".type words_equal, @function\n"
+//     "words_equal:\n"
+//     "xor rcx, rcx\n"
+//     "StrcmpLoop:\n"
+//         "xor rcx, rcx\n"
+//         "mov cl, byte ptr [rdi]\n"
+//         "cmp cl, byte ptr [rdx]\n"
+//         "jne LoopExit\n"
+//         "inc rdi\n"
+//         "inc rdx\n"
+//         "cmp cl, 0\n"
+//         "jne StrcmpLoop\n"
+//         "mov rax, 0xffff\n"
+//         "ret\n"
+//     "LoopExit:\n"
+//     "xor rax, rax\n"
+//     "ret\n"
+//     ".att_syntax noprefix\n"
+// );
 
 void DestroyElement(Word word) {
     free(word.word);
